0073e0d16eaf538f0dbb538d9cce3f92a2c7af64
Josh Burrill <joshburrill@Joshs-MacBook-Pro.local>
2020-04-10 13:51:21 +0100

    Adding registration function to frontend

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,42 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password1: "",
+    password2: "",
+  });
+
+  const { name, email, password1, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password1 !== password2) {
+      console.log("Passwords do not match");
+    } else {
+      const newUser = {
+        name,
+        email,
+        password1,
+      };
+
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+
+        const body = JSON.stringify(newUser);
+
+        const res = await axios.post("/api/users", body, config);
+        console.log(res.data);
+      } catch (err) {
+        console.error(err.response.data);
+      }
+    }
+  };
+
   return (
     <>
-      <h1 class='large text-primary'>Sign Up</h1>
-      <p class='lead'>
-        <i class='fas fa-user'></i> Create Your Account
+      <h1 className='large text-primary'>Sign Up</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Create Your Account
       </p>
-      <form class='form' action='create-profile.html'>
-        <div class='form-group'>
-          <input type='text' placeholder='Name' name='name' required />
+      <form
+        className='form'
+        action='create-profile.html'
+        onSubmit={(e) => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Name'
+            value={name}
+            onChange={(e) => onChange(e)}
+            name='name'
+            required
+          />
         </div>
-        <div class='form-group'>
-          <input type='email' placeholder='Email Address' name='email' />
-          <small class='form-text'>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            required
+            value={email}
+            onChange={(e) => onChange(e)}
+            name='email'
+          />
+          <small className='form-text'>
             This site uses Gravatar so if you want a profile image, use a
             Gravatar email
           </small>
         </div>
-        <div class='form-group'>
+        <div className='form-group'>
           <input
             type='password'
             placeholder='Password'
-            name='password'
+            name='password1'
             minLength='6'
+            value={password1}
+            onChange={(e) => onChange(e)}
           />
         </div>
-        <div class='form-group'>
+        <div className='form-group'>
           <input
             type='password'
             placeholder='Confirm Password'
             name='password2'
             minLength='6'
+            value={password2}
+            onChange={(e) => onChange(e)}
           />
         </div>
-        <input type='submit' class='btn btn-primary' value='Register' />
+        <input type='submit' className='btn btn-primary' value='Register' />
       </form>
-      <p class='my-1'>
+      <p className='my-1'>
         Already have an account? <a href='login.html'>Sign In</a>
       </p>
     </>
