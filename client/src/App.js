cbe13dac2608e054760f94bc359ecbfc7b3e96e2
Josh Burrill <joshburrill@Joshs-MacBook-Pro.local>
2020-04-14 15:42:14 +0100

    Update profile function added

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import { loadUser } from "./actions/auth";
 import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-forms/CreateProfile";
 import PrivateRoute from "./components/routing/PrivateRoute";
+import EditProfile from "./components/profile-forms/EditProfile";
 
 // Redux
 import { Provider } from "react-redux";
@@ -36,6 +37,11 @@ const App = () => {
                 exact
                 path='/create-profile'
                 component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path='/edit-profile'
+                component={EditProfile}
               />
             </Switch>
           </section>
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -49,7 +49,6 @@ export const createProfile = (formData, history, edit = false) => async (
     }
   } catch (err) {
     const errors = err.response.data.errors;
-    console.log(`Errors: ${err.response.data.errors}`);
 
     if (errors) {
       errors.forEach((error) => dispatch(setAlert(error.msg, "danger")));
